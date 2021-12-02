5b8f9ad20149c0c374fcdf137bfe6465d4fe8342
Gabriella Emerson <2016045@folkestonegirls.kent.sch.uk>
2021-12-02 12:17:42 +0000

    Started solving day 1 part 1

--- PuzzleSolutions/Y2021/Y2021-D1.cs
+++ PuzzleSolutions/Y2021/Y2021-D1.cs
@@ -10,6 +10,16 @@ namespace PuzzleSolutions.Y2021
         public static string SolvePart1()
         {
             List<int> puzzleInput = MyIO.ReadIntsFromFile(2021, 1, 1);
+            int length = puzzleInput.Count;
+            int increased = 0;
+            for (int i = 1; i < length; i++)
+            {
+                if (puzzleInput[i] > puzzleInput[i - 1])
+                {
+                    increased += 1;
+                }
+
+            }
             return "I Haven't Solved part 1 yet!";
         }
 
